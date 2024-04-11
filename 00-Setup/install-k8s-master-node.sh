35e5a81c7301fe9aad175bebb7368f1f36cd8871
Amit Vashist <amitvashist7@outlook.com>
2024-04-11 06:28:07 +0000

    13-Taint-and-toleration-with-affinity

--- 00-Setup/install-k8s-master-node.sh
+++ 00-Setup/install-k8s-master-node.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-#echo "********************  Installing Docker ***********************"
-#sleep 3
-#apt-get update
-#apt-get install -y \
-#    apt-transport-https \
-#    ca-certificates \
-#    curl \
-#    software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-#add-apt-repository \
-#   "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-#   $(lsb_release -cs) \
-#   stable"
-#apt-get update && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 17.03 | head -1 | awk '{print $3}')
+echo "******************** Installing CRIO ***********************"
 
-
-echo "********************  Installing CRIO ***********************"
 apt-get update
 apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
 
 swapoff -a
 
@@ -32,13 +17,15 @@ overlay
 br_netfilter
 EOF
 
+
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
+
 # Set up required sysctl params, these persist across reboots.
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward         = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
@@ -52,13 +39,16 @@ VERSION=1.23
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | \
 sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 
+
 # Import Public Key
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | \
 sudo apt-key add -
 
+
 # Add CRI Repo
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | \
 sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
 
 # Import Public Key
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | \
@@ -66,43 +56,34 @@ sudo apt-key add -
 
 
 
+
 sudo apt update
 sudo apt install cri-o cri-o-runc cri-tools -y
 
 sudo systemctl enable crio.service
-
 sudo systemctl start crio.service
-
 sudo crictl info
 
 
+
 echo "******************** Installing kubernetes *********************"
-#apt-get update && apt-get install -y apt-transport-https
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-#deb http://apt.kubernetes.io/ kubernetes-xenial main
-#EOF
-#apt-get update
-#apt-get install -y kubelet kubeadm kubectl
 
 
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor | sudo dd status=none of=/usr/share/keyrings/kubernetes-archive-keyring.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+
+
 echo "******************* Deploying kubernetes ***********************"
 
 # Fix Me - Update the "apiserver-advertise-address" with your master node ip.
-
 kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address="172.31.0.100" --cri-socket unix://var/run/crio/crio.sock
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
--- 00-Setup/install-k8s-worker-node.sh
+++ 00-Setup/install-k8s-worker-node.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "********************  Installing CRIO ***********************"
+echo "******************** Installing CRIO ***********************"
+
 apt-get update
 apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
 
 swapoff -a
 
@@ -16,13 +17,15 @@ overlay
 br_netfilter
 EOF
 
+
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
+
 # Set up required sysctl params, these persist across reboots.
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward         = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
@@ -36,13 +39,16 @@ VERSION=1.23
 echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" | \
 sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 
+
 # Import Public Key
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | \
 sudo apt-key add -
 
+
 # Add CRI Repo
 echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" | \
 sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
 
 # Import Public Key
 curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | \
@@ -50,36 +56,28 @@ sudo apt-key add -
 
 
 
+
 sudo apt update
 sudo apt install cri-o cri-o-runc cri-tools -y
 
 sudo systemctl enable crio.service
-
 sudo systemctl start crio.service
-
 sudo crictl info
 
 
+
 echo "******************** Installing kubernetes *********************"
-#apt-get update && apt-get install -y apt-transport-https
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-#deb http://apt.kubernetes.io/ kubernetes-xenial main
-#EOF
-#apt-get update
-#apt-get install -y kubelet kubeadm kubectl
 
 
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-#echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor | sudo dd status=none of=/usr/share/keyrings/kubernetes-archive-keyring.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+
+
 
